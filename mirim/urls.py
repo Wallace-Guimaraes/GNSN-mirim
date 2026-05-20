294abc27e80ef17ea6a26000b418e65844165ae0
Wallace-Guimaraes <Wallaceluan4533@gmail.com>
2026-05-20 16:33:59 -0300

    Primeiras telas

---
+++ mirim/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from mirim import views
+
+app_name = "mirim"
+
+urlpatterns = [
+    path("home/", views.home, name="home"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("frequencia/", views.frequencia, name="frequencia"),
+    path("guardas/", views.listaGuarda, name="guardas"),
+    path("cadastro-evento/", views.cadastroEvento, name="cadastro_evento"),
+    path("eventos/", views.eventos, name="eventos"),
+]
---
+++ mirim/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("home")
+
+def dashboard(request):
+    return render(request, "mirim/dashboard.html")
+
+def frequencia(request):
+    return render(request, "mirim/frequencia.html")
+
+def listaGuarda(request):
+    return render(request, "mirim/listaGuarda.html")
+    
+def cadastroEvento(request):
+    return render(request, "mirim/cadastroEvento.html")
+
+def eventos(request):
+    return render(request, "mirim/eventos.html")
+ 
+ 
